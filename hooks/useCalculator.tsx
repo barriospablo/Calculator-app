809a24447ac10296c01280775ef44cc789a909bd
Pablo Barrios <exepablo96@gmail.com>
2025-08-13 12:53:11 -0300

    useCalculator hook

---
+++ hooks/useCalculator.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useRef, useState } from "react";
+
+enum Operator {
+  add = "+",
+  subtract = "-",
+  multiply = "*",
+  divide = "/",
+}
+
+export const useCalculator = () => {
+  const [formula, setFormula] = useState("");
+
+  const [number, setNumber] = useState("0");
+  const [prevNumber, setPrevNumber] = useState("0");
+
+  const lastOperation = useRef<Operator>();
+
+  useEffect(() => {
+    setFormula(number);
+  }, [number]);
+
+  const clean = () => {
+    setNumber("0");
+    setPrevNumber("0");
+    setFormula("0");
+
+    lastOperation.current = undefined;
+  };
+
+  const toggleSign = () => {
+    if (number.includes("-")) {
+      return setNumber(number.replace("-", ""));
+    }
+    setNumber("-" + number);
+  };
+  const deleteLast = () => {
+    //My solution
+    // if (number.length > 0 && number !== "0") {
+    //   setNumber(number.slice(0, -1));
+    // }
+    // if (number.length === 1) {
+    //   setNumber("0");
+    // }
+    let currentSign = "";
+    let temporalNumber = number;
+    if (number.includes("-")) {
+      currentSign = "-";
+      temporalNumber = number.substring(1);
+    }
+    if (temporalNumber.length > 1) {
+      return setNumber(currentSign + temporalNumber.slice(0, -1));
+    }
+    setNumber("0");
+  };
+
+  const buildNumber = (numberString: string) => {
+    //Verificar si ya existe el punto decimal
+    if (number.includes(".") && numberString === ".") return;
+
+    if (number.startsWith("0") || number.startsWith("-0")) {
+      if (numberString === ".") {
+        return setNumber(number + numberString);
+      }
+
+      //Evaluar si es otro cero y no hay punto
+      if (numberString === "0" && number.includes(".")) {
+        return setNumber(number + numberString);
+      }
+      //Evaluar si es diferente de cero, no hay punto y es el primer numero
+      if (numberString !== "0" && !number.includes(".")) {
+        return setNumber(numberString);
+      }
+
+      //Evitar el 000000
+      if (numberString === "0" && !number.includes(".")) {
+        return;
+      }
+      return setNumber(number + numberString);
+    }
+    return setNumber(number + numberString);
+  };
+
+  return {
+    //Props
+    formula,
+    number,
+    prevNumber,
+    //Methods
+    buildNumber,
+    clean,
+    toggleSign,
+    deleteLast,
+  };
+};
