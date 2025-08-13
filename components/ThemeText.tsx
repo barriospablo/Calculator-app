ba6dd1de0a3f289c2a436e5f60e80601786f71d3
Pablo Barrios <exepablo96@gmail.com>
2025-08-12 23:00:15 -0300

    add text component

---
+++ components/ThemeText.tsx
@@ -0,0 +1,27 @@
+import { globalStyles } from "@/styles/global-styles";
+import React from "react";
+import { Text, type TextProps } from "react-native";
+
+interface Props extends TextProps {
+  variant: "h1" | "h2";
+  //   children: string;
+}
+
+const ThemeText = ({ children, variant, ...rest }: Props) => {
+  return (
+    <Text
+      style={[
+        { color: "white", fontFamily: "SpaceMono" },
+        variant === "h1" && globalStyles.mainResult,
+        variant === "h2" && globalStyles.subResult,
+      ]}
+      numberOfLines={1}
+      adjustsFontSizeToFit
+      {...rest}
+    >
+      {children}
+    </Text>
+  );
+};
+
+export default ThemeText;
